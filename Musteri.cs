76acd1831822611a0291bac661043130e098edc7
nazanbahar <77123580+nazanbahar@users.noreply.github.com>
2021-01-20 15:29:37 +0300

    Add files via upload

---
+++ Musteri.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        //id Category  IbanNo TCKimlikNo KrediNotu DogumTarihi MusteriAdi MusteriSoyadi MusteriTelefon  MusteriAdres
+
+        public int Id { get; set; }
+        public int Category { get; set; }
+        public string IbanNo { get; set; }
+        public int TCKimlikNo { get; set; }
+        public int KrediNotu { get; set; }
+        public string MusteriAdi { get; set; }
+        public string MusteriSoyadi { get; set; }
+        public int MusteriTelefon { get; set; }
+        public string MusteriAdres { get; set; }
+        public double MusteriBakiye { get; set; }
+
+
+
+
+    }
+}
